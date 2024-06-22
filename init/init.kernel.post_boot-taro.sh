bbeead0009c28e9bdc5c70ead713f4623a2b510c
AgBKartikey <kartikey.np2@gmail.com>
2024-06-22 18:12:52 +0000

    Revert "Pong: init: Adjust foreground cpuset"
    This reverts commit a7f06c394a9abfbddbfac28a6d9a9bf979dc5874.

--- init/init.kernel.post_boot-taro.sh
+++ init/init.kernel.post_boot-taro.sh
@@ -82,7 +82,6 @@ echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
 echo 0-3 > /dev/cpuset/background/cpus
-echo 0-6 > /dev/cpuset/foreground/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
 
 # Turn off scheduler boost at the end
